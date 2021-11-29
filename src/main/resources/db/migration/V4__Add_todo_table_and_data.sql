0ec902df5cddae17083861a2eeabd07209c513ba
Michał Grzegórzek <michal.grzegorzek33@gmail.com>
2021-11-29 08:22:06 +0100

    Adding files

---
+++ src/main/resources/db/migration/V4__Add_todo_table_and_data.sql
@@ -0,0 +1,8 @@
+create table todos (
+id int unsigned primary key auto_increment,
+text varchar(100) not null,
+done bit
+);
+
+insert into todos (text, done) values ('Done to do', 1);
+insert into todos (text, done) values ('Undone to do', 0);
